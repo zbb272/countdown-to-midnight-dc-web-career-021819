87ae96a77d17dfe71e1aaffa388cca80fcd7dd2a
Zachary <zbberman@yahoo.com>
2019-01-29 16:23:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown (number)
   end 
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end 
+  "HAPPY NEW YEAR!"
+end
